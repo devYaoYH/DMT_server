7065ae770830bf84363e13d8dad0cbd1c02809b6
DevYaoYH <yaoyiheng@gmail.com>
2019-10-27 07:43:36 +0000

    Working clien/server pair

--- db_client.py
+++ db_client.py
@@ -1,16 +1,24 @@
+import sys
 import socket
 import pickle
 import json
 
 BUF_SIZE = 16
-PKT_HEADER = 2
+PKT_HEADER = 4
 
 def run_client(ADDR):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect(ADDR)
-        s.sendall(pickle.dumps(json.dumps({'data': "stuff", 'label': [0, 2, 23, 4, 2, 1]})))
+        send_bytes = pickle.dumps(json.dumps({'args': "" if len(sys.argv) < 2 else sys.argv[1], 'label': [0, 2, 23, 4, 2, 1]}))
+        send_len = bytearray()
+        send_len.append((len(send_bytes)>> 24)&255)
+        send_len.append((len(send_bytes)>> 16)&255)
+        send_len.append((len(send_bytes)>> 8)&255)
+        send_len.append(len(send_bytes)&255)
+        s.send(send_len)
+        s.sendall(send_bytes)
         pkt_size = bytearray(s.recv(PKT_HEADER))
-        pkt_size = (int(pkt_size[0]) << 8) + int(pkt_size[1])
+        pkt_size = (int(pkt_size[0]) << 24) + (int(pkt_size[1]) << 16) + (int(pkt_size[2]) << 8) + int(pkt_size[3])
         print(f"Client receiving {pkt_size} bytes")
         ret_pkt = bytearray()
         data = s.recv(min(BUF_SIZE, pkt_size - len(ret_pkt)))
@@ -21,6 +29,28 @@ def run_client(ADDR):
             data = s.recv(min(BUF_SIZE, pkt_size - len(ret_pkt)))
             print("client:", data)
         print("Echo:", pickle.loads(ret_pkt))
+
+# Our pkt is a json.dumps object
+def send_pkt(ADDR, pkt):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect(ADDR)
+        send_bytes = pickle.dumps(pkt)
+        send_len = bytearray()
+        send_len.append((len(send_bytes)>> 24)&255)
+        send_len.append((len(send_bytes)>> 16)&255)
+        send_len.append((len(send_bytes)>> 8)&255)
+        send_len.append(len(send_bytes)&255)
+        s.send(send_len)
+        s.sendall(send_bytes)
+        pkt_size = bytearray(s.recv(PKT_HEADER))
+        pkt_size = (int(pkt_size[0]) << 24) + (int(pkt_size[1]) << 16) + (int(pkt_size[2]) << 8) + int(pkt_size[3])
+        print(f"Client receiving {pkt_size} bytes")
+        ret_pkt = bytearray()
+        data = s.recv(min(BUF_SIZE, pkt_size - len(ret_pkt)))
+        while len(ret_pkt) < pkt_size:
+            ret_pkt.extend(data)
+            data = s.recv(min(BUF_SIZE, pkt_size - len(ret_pkt)))
+        print("Received pkt:", pickle.loads(ret_pkt))
 
 if __name__ == '__main__':
     HOST = 'localhost'
--- db_server.py
+++ db_server.py
@@ -2,6 +2,9 @@ import socket
 import pickle
 import json
 
+BUF_SIZE = 12
+PKT_HEADER = 4
+
 def run_server(ADDR):
     host, port = ADDR
 
@@ -14,19 +17,24 @@ def run_server(ADDR):
         conn, addr = server_socket.accept()
         with conn:
             print("connected by:", addr)
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                send_pkt = pickle.dumps(data)
-                send_pkt_len = len(send_pkt)
-                print(f"Server sending {send_pkt_len} bytes")
-                pkt_len = bytearray()
-                pkt_len.append((send_pkt_len >> 8)&255)
-                pkt_len.append(send_pkt_len&255)
-                print(f"Sending {pkt_len} bytes")
-                conn.send(pkt_len)
-                conn.sendall(data)
+            recv_pkt = bytearray()
+            recv_size = bytearray(conn.recv(PKT_HEADER))
+            recv_size = (int(recv_size[0]) << 24) + (int(recv_size[1]) << 16) + (int(recv_size[2]) << 8) + int(recv_size[3])
+            while len(recv_pkt) < recv_size:
+                data = conn.recv(min(BUF_SIZE, recv_size - len(recv_pkt)))
+                recv_pkt.extend(data)
+            print(f"Server received {recv_pkt}")
+            send_pkt = recv_pkt
+            send_pkt_len = len(send_pkt)
+            print(f"Server sending {send_pkt_len} bytes")
+            pkt_len = bytearray()
+            pkt_len.append((send_pkt_len >> 24)&255)
+            pkt_len.append((send_pkt_len >> 16)&255)
+            pkt_len.append((send_pkt_len >> 8)&255)
+            pkt_len.append(send_pkt_len&255)
+            print(f"Server sending {pkt_len} bytes")
+            conn.send(pkt_len)
+            conn.sendall(send_pkt)
 
 if __name__ == '__main__':
     HOST = 'localhost'
